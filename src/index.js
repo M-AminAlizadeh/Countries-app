67a3090b982a163489bebfece97bcf2a666e606d
M-AminAlizadeh <mohammadaminalaizadeh1378@gmail.com>
2023-09-06 15:44:31 +0330

    Add redux to react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const root = document.querySelector('#root');
 
 ReactDom.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   root,
 );

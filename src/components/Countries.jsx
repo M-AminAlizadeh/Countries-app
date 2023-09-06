f1b46b4d30f072ba1fad38d22b045d6d28fe27f3
M-AminAlizadeh <mohammadaminalaizadeh1378@gmail.com>
2023-09-06 15:45:44 +0330

    Render fetched data to UI and style it

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,3 +1,50 @@
-const Countries = () => 'countries';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchCountries } from '../redux/countries/countriesSlice';
+import styles from "./countries.module.css";
+
+function Countries() {
+  const dispatch = useDispatch();
+  const countries = useSelector((state) => state.countries.countries);
+  const loading = useSelector((state) => state.countries.loading);
+  const error = useSelector((state) => state.countries.error);
+
+  useEffect(() => {
+    dispatch(fetchCountries());
+  }, [dispatch]);
+
+if(loading){
+  return (<div>Loading ...</div>)
+}else if (error){
+  return (<div>{error.message}</div>)
+}else{
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.header}>List of Countries</h1>
+      <div className={styles.countriesContainer}>
+      {countries.map((country)=>{
+        return(
+          <div className={styles.countryContainer} key={country.name.common}>
+              <img src={country.flags.svg} alt={country.flags.alt} className={styles.flag}/>
+              <span className={styles.commonName}>
+                Common Name: {country.name.common}
+              </span>
+              <span className={styles.capitalName}>
+                Capital Name: {country.capital}
+              </span>
+              <span className={styles.population}>
+                Population: {country.population}
+              </span>
+  
+          </div>
+        )
+      })}
+      </div>
+    </div>
+  );
+
+}
+
+}
 
 export default Countries;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Navbar from '../components/Navbar';
 import Searchbox from '../components/Searchbox';
+import Countries from '../components/Countries';
 
 const Home = () => (
   <div className="homeContainer">
     <Navbar />
     <Searchbox />
+    <Countries />
   </div>
 );
 

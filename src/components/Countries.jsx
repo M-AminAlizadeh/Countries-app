597b0d66fbffc16b98be341e79b59b434e037e1c
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-09-07 12:37:49 +0330

    Add searchbox functionality and link it to countries component

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchCountries } from '../redux/countries/countriesSlice';
+import { selectSearchTerm } from '../redux/searchbox/searchboxSlice';
 import styles from './countries.module.css';
 
 function Countries() {
@@ -9,10 +10,15 @@ function Countries() {
   const countries = useSelector((state) => state.countries.countries);
   const loading = useSelector((state) => state.countries.loading);
   const error = useSelector((state) => state.countries.error);
+  const searchTerm = useSelector(selectSearchTerm);
 
   useEffect(() => {
     dispatch(fetchCountries());
   }, [dispatch]);
+
+  const filteredCountries = countries.filter(
+    (country) => country.name.common.toLowerCase().includes(searchTerm.toLowerCase()),
+  );
 
   if (loading) {
     return (<div>Loading ...</div>);
@@ -23,7 +29,7 @@ function Countries() {
     <div className={styles.container}>
       <h1 className={styles.header}>List of Countries</h1>
       <div className={styles.countriesContainer}>
-        {countries.map((country) => (
+        {filteredCountries.map((country) => (
           <div className={styles.countryContainer} key={country.name.common}>
             <img src={country.flags.svg} alt={country.flags.alt} className={styles.flag} />
             <span className={styles.commonName}>
